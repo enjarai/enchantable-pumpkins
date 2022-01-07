19c468c30db9d379d95fc6fa334a65448ff10e99
enjarai <enjarai123@gmail.com>
2022-01-07 14:13:49 +0100

    initial

---
+++ src/main/java/nl/enjarai/enchantablepumpkins/mixin/EnchantmentTargetHeadArmorMixin.java
@@ -0,0 +1,18 @@
+package nl.enjarai.enchantablepumpkins.mixin;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(targets = "net/minecraft/enchantment/EnchantmentTarget$10")
+public class EnchantmentTargetHeadArmorMixin {
+    @Inject(method = "isAcceptableItem", at = @At("HEAD"), cancellable = true)
+    private void modifyAcceptableItem(Item item, CallbackInfoReturnable<Boolean> cir) {
+        if (item == Items.CARVED_PUMPKIN) {
+            cir.setReturnValue(true);
+        }
+    }
+}
